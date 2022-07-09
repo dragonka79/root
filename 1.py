54f9cfa6eb5c7a09fee2131f9b4ba3b25661d486
dragonka79 <dragonka79@gmail.com>
2022-07-09 17:44:46 +0200

    na

--- 1.py
+++ 1.py
@@ -1,8 +1,12 @@
-s = -9 //4
-print(s)
 
-#  Szabályos egész érték számítás
+# This program converts seconds to hours, minutes and seconds
 
-l = -9 /4
-r = round(l)
-print(r)
+seconds = int(input('Give me the seconds > '))
+
+hours = seconds // 3600
+seconds_leftover_hours = seconds % 3600
+minutes = seconds_leftover_hours // 60
+seconds_leftover_minutes = seconds_leftover_hours % 60
+
+print(f"""The {seconds} seconds equals to {hours} hours, {minutes} minutes 
+and {seconds_leftover_minutes} seconds""")
