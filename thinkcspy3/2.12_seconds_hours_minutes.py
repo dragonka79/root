66d4448af925a5a8af8be399030506e1a160a85c
dragonka79 <dragonka79@gmail.com>
2022-07-09 17:44:20 +0200

    creation

---
+++ thinkcspy3/2.12_seconds_hours_minutes.py
@@ -0,0 +1,11 @@
+# This program converts seconds to hours, minutes and seconds
+
+seconds = int(input('Give me the seconds > '))
+
+hours = seconds // 3600
+seconds_leftover_hours = seconds % 3600
+minutes = seconds_leftover_hours // 60
+seconds_leftover_minutes = seconds_leftover_hours % 60
+
+print(f"""The {seconds} seconds equals to {hours} hours, {minutes} minutes 
+and {seconds_leftover_minutes} seconds""")
