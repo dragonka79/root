dc0bc7757e82d429be23edbab69724b9a15622ed
dragonka79 <dragonka79@gmail.com>
2022-07-10 08:15:35 +0200

    fix

--- thinkcspy3/3/3.1.2.py
+++ thinkcspy3/3/3.1.2.py
@@ -1,15 +1,15 @@
 import turtle
 
 color = str(input("Give me the color of the background of the window > "))
-pensize = str(input("Give me the pensize of the turtle > "))
+pencolor = str(input("Give me the pensize of the turtle > "))
 
 ablak = turtle.Screen()
 ablak.bgcolor(color) # Állítsd be az ablak háttérszínét!
 ablak.title("Hello, Eszti!") # Állítsd be az ablak címét!
 
 Eszti = turtle.Turtle()
-Eszti.color("blue") # Mond meg Esztinek, hogy változtasson színt!
-Eszti.pensize(pensize) # Mond meg Esztinek, hogy változtassa meg a tolla vastagságát!
+Eszti.color(pencolor) # Mond meg Esztinek, hogy változtasson színt!
+Eszti.pensize(3) # Mond meg Esztinek, hogy változtassa meg a tolla vastagságát!
 Eszti.forward(50)
 Eszti.left(120)
 Eszti.forward(50)
