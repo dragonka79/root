158379e5804e0072154e218792d27a9702c2241d
dragonka79 <dragonka79@gmail.com>
2022-07-10 07:40:09 +0200

    creation

---
+++ thinkcspy3/3/3.1.1.py
@@ -0,0 +1,16 @@
+import turtle
+
+color = str(input("Give me the color of the background of the window > "))
+
+ablak = turtle.Screen()
+ablak.bgcolor(color) # Állítsd be az ablak háttérszínét!
+ablak.title("Hello, Eszti!") # Állítsd be az ablak címét!
+
+Eszti = turtle.Turtle()
+Eszti.color("blue") # Mond meg Esztinek, hogy változtasson színt!
+Eszti.pensize(3) # Mond meg Esztinek, hogy változtassa meg a tolla vastagságát!
+Eszti.forward(50)
+Eszti.left(120)
+Eszti.forward(50)
+
+ablak.mainloop()
