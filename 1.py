f223cf3a6fd65ca1b3156d9f2b67720d10399095
dragonka79 <dragonka79@gmail.com>
2022-07-10 07:40:43 +0200

    na

--- 1.py
+++ 1.py
@@ -1,7 +1,16 @@
-a = "Lustaság"
-b = "fél"
-c = "egészség"
+import turtle
 
-print(a + ' ' + b + ' ' + c)
+color = str(input("Add meg a háttérszínt > "))
 
+ablak = turtle.Screen()
+ablak.bgcolor(color) # Állítsd be az ablak háttérszínét!
+ablak.title("Hello, Eszti!") # Állítsd be az ablak címét!
 
+Eszti = turtle.Turtle()
+Eszti.color("blue") # Mond meg Esztinek, hogy változtasson színt!
+Eszti.pensize(3) # Mond meg Esztinek, hogy változtassa meg a tolla vastagságát!
+Eszti.forward(50)
+Eszti.left(120)
+Eszti.forward(50)
+
+ablak.mainloop()
