07a5c8158bf5d993844aaeebcf25c9020a651a18
dragonka79 <dragonka79@gmail.com>
2022-07-11 11:18:37 +0200

    na

--- 1.py
+++ 1.py
@@ -1,6 +1,17 @@
 import turtle
 
+a = turtle.Screen()
 
-Eszti = turtle.Turtle()
-v = Eszti
-print(type(v))
+def sokszög_rajzolasa(t, h, s):
+    """
+    Egy s oldalú h oldalhosszúságú sokszög rajzolása a t teknőccel
+    """
+    for i in range(s):
+        t.forward(h)
+        t.left(360/s)
+
+s = 8
+Zoli = turtle.Turtle()
+sokszög_rajzolasa(Zoli, 50, s)
+
+a.mainloop()
