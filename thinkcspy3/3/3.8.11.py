922d0446eb5d5f40369be4e38b43bf137de27fff
dragonka79 <dragonka79@gmail.com>
2022-07-11 08:47:21 +0200

    creation

---
+++ thinkcspy3/3/3.8.11.py
@@ -0,0 +1,14 @@
+import turtle
+
+ablak = turtle.Screen()
+Zoli = turtle.Turtle()
+Zoli.pensize(3)
+Zoli.speed(2)
+Zoli.right(72)
+
+for i in range(5):
+    Zoli.forward(300)
+    Zoli.right(144)
+
+
+ablak.mainloop()
