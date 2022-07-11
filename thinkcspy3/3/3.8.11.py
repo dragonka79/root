ed51dff07d2d683c8b86e45b25d8adccfbe1a240
dragonka79 <dragonka79@gmail.com>
2022-07-11 08:50:38 +0200

    hide/show turtle entries

--- thinkcspy3/3/3.8.11.py
+++ thinkcspy3/3/3.8.11.py
@@ -5,10 +5,11 @@ Zoli = turtle.Turtle()
 Zoli.pensize(3)
 Zoli.speed(2)
 Zoli.right(72)
+Zoli.hideturtle()
 
 for i in range(5):
     Zoli.forward(300)
     Zoli.right(144)
 
-
+Zoli.showturtle()
 ablak.mainloop()
