9c54a6a3ce91b5eec9479a74dedb69dc801f461f
dragonka79 <dragonka79@gmail.com>
2022-07-11 09:36:07 +0200

    na

--- 1.py
+++ 1.py
@@ -1,18 +1,6 @@
 import turtle
 
-ablak = turtle.Screen()
-ablak.bgcolor("lightgreen")
 
 Eszti = turtle.Turtle()
-Eszti.shape("turtle")
-Eszti.color("blue")
-Eszti.speed(2)
-Eszti.penup() # Ez új
-meret = 20
-for i in range(30):
-    Eszti.stamp()
-    meret = meret + 3 # Növeld a méretet minden ismétlésnél!
-    Eszti.forward(meret) # Mozgasd ...
-    Eszti.right(24) # ... és fordítsd Esztit!
-
-ablak.mainloop()
+v = Eszti
+print(type(v))
