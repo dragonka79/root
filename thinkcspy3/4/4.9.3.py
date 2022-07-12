ee8214aaea8789efba5febde029d81c52e0de3af
dragonka79 <dragonka79@gmail.com>
2022-07-12 14:20:42 +0200

    creation

---
+++ thinkcspy3/4/4.9.3.py
@@ -0,0 +1,23 @@
+import turtle
+
+a = turtle.Screen()
+a.bgcolor("lightgreen")
+
+t = turtle.Turtle()
+t.color("deeppink")
+t.pensize(3)
+t.speed(1)
+
+def sokszog_rajzolas(t, n, sz):
+    """ Szabályos n-szög rajzolása"""
+    for i in range(n):
+        t.forward(sz)
+        t.left(360 /n )
+
+Eszti = t
+n = 8
+sz = 50
+
+sokszog_rajzolas(t, n, sz)   
+
+a.mainloop()
