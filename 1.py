c2608fe4d85b383718c149815ec3ba505e616cc1
dragonka79 <dragonka79@gmail.com>
2022-07-12 16:42:36 +0200

    na

--- 1.py
+++ 1.py
@@ -8,25 +8,14 @@ t = turtle.Turtle()
 t.color("blue")
 t.pensize(3)
 t.speed(0)
-t.hideturtle()
+# t.hideturtle()
 
 def minta(sz):
-    for i in range(2):
-        for i in range(4):
-            t.forward(sz)
-            t.left(90)
+    for i in range(1, 96):
         t.right(90)
-    for i in range(4):
-        t.forward(sz)
-        t.right(90)
-    for i in range(4):
-        t.forward(sz)
-        t.left(90)
-    t.right(165)
+        t.forward(i * sz)
+    t.right(90)
 
-sz = 150
-
-for i in range(6):
-    minta(sz)
-
+sz = 4
+minta(sz)
 a.mainloop()
