75c4ecfcac2319a6aea51251ad32b265c2efa7e5
dragonka79 <dragonka79@gmail.com>
2022-07-13 20:12:49 +0200

    na

--- 1.py
+++ 1.py
@@ -1,23 +1,4 @@
-def derekszogu_e(a, b, c):
-    """Meghatároza, hogy a háromszög derékszögű-e"""
-
-    # Háromszög létezés feltétele
-    if ((a > 0 and b > 0 and c > 0)) and \
-        (abs(a + b - c > 0.000001) or \
-         abs(a + c - b > 0.000001) or \
-         abs(b + c - a > 0.000001)):
-    # A derékszögű feltétel
-        if abs(a ** 2 + b ** 2 - c ** 2 < 0.000001) or \
-           abs(a ** 2 + c ** 2 - b ** 2 < 0.000001) or \
-           abs(b ** 2 + c ** 2 - a ** 2 < 0.000001):
-           return True
-        else:
-            return False
-    else:
-        v = "A három adat nem háromszöget határoz meg"
-        return v
-        
-
-(a, b, c) = (5, 12, 5)
-
-print(derekszogu_e(a, b ,c))
+import math
+a = math.sqrt(2.0)
+print(a, a*a)
+print(a*a == 2.0)
