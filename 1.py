5175ea1649ca0264cdc3f8557be1d401c4cf1589
dragonka79 <dragonka79@gmail.com>
2022-07-13 06:02:10 +0200

    na

--- 1.py
+++ 1.py
@@ -1,21 +1,11 @@
-import turtle
+from math import sqrt
 
-a = turtle.Screen()
-a.bgcolor("lightgreen")
-a.title("Gyönyörű minta")
+x = 2
+def gyok(x):
+    if x < 0:
+        print("Nono")
+        return
+    else:
+        print(sqrt(x))
 
-t = turtle.Turtle()
-t.color("blue")
-t.pensize(3)
-t.speed(0)
-# t.hideturtle()
-
-def minta(sz):
-    for i in range(1, 96):
-        t.right(90)
-        t.forward(i * sz)
-    t.right(90)
-
-sz = 4
-minta(sz)
-a.mainloop()
+gyok(x)
