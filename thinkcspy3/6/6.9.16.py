5c75bd202278c50bd1413d86794b08281516332f
dragonka79 <dragonka79@gmail.com>
2022-07-15 07:35:54 +0200

    fix

---
+++ thinkcspy3/6/6.9.16.py
@@ -0,0 +1,25 @@
+from unittest import teszt
+
+def tenyezo_e(t, n):
+    """n t-nek egész számszorosa-e"""
+    try:
+        if n % t == 0:
+            return True
+        else:
+            return False
+    except:
+        return "Zero division"
+
+
+def tesztkeszlet_tenyezo_e():
+    """ Az ehhez a modulhoz tartozó tesztkészlet. """
+    teszt(tenyezo_e(3, 12))
+    teszt(not tenyezo_e(5, 12))
+    teszt(tenyezo_e(7, 14))
+    teszt(not tenyezo_e(7, 15))
+    teszt(tenyezo_e(1, 15))
+    teszt(tenyezo_e(15, 15))
+    teszt(not tenyezo_e(25, 15))
+    teszt(tenyezo_e(0, 15) == "Zero division")
+
+tesztkeszlet_tenyezo_e()
