d0499808451eba71a5e2bac7078c8404d697fafd
dragonka79 <dragonka79@gmail.com>
2022-07-18 18:38:07 +0200

    fix

--- thinkcspy3/7/7.26.1.py
+++ thinkcspy3/7/7.26.1.py
@@ -2,6 +2,7 @@ from unittest import teszt
 
 
 def paratlan_szamok_szama(lista):
+    """Gives the odd numbers in a list"""
     counter = 0
     for i in lista:
         if i % 2 != 0:
@@ -11,8 +12,8 @@ def paratlan_szamok_szama(lista):
 
 
 def tesztkeszlet():
-    """ Az ehhez a modulhoz (fájlhoz) tartozó tesztkészlet futtatása.
-    """
+    """ Az ehhez a modulhoz (fájlhoz) tartozó tesztkészlet futtatása."""
+    
     teszt(paratlan_szamok_szama([1, 2, 3, 4, -5]) == 3)
     teszt(paratlan_szamok_szama([0]) == 0)
     teszt(paratlan_szamok_szama([12, -24, 36, -108]) == 0)
