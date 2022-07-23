6554987ac011e9a6146a4b14a673cdddd6916165
dragonka79 <dragonka79@gmail.com>
2022-07-23 11:02:29 +0200

    fix

--- thinkcspy3/7/7.26.17._ko_papir_ollo.py
+++ thinkcspy3/7/7.26.17._ko_papir_ollo.py
@@ -4,23 +4,25 @@ import random
 folytat = 'y'
 print("""Ez a kő-papír-olló játék a számítógép ellen.
         Kő = 1
-        Papír = 0
-        Olló = -1
+        Papír = 2
+        Olló = 3
         """)
 
 gep = 0
 user = 0
 egal = 0
+count = 0
 
 while folytat != 'n':
+    count += 1
     try:
-        u = int(input("Kő, papír vagy olló ( 1 / 0 /- 1 ?")) # user tipp
-        if u in [1, 0, -1]:
-            v = random.randrange(-1, 2) # gép tipp
+        u = int(input("Kő, papír vagy olló ( 1 / 2 / 3 )?")) # user tipp
+        if u in [1, 2, 3]:
+            v = random.randrange(1, 4) # gép tipp
             if v != u:
-                if (u == 1 and v == -1) or \
-                    (u == 0 and v == 1) or \
-                   (u == -1 and u == -0):
+                if (u == 1 and v == 3) or \
+                    (u == 2 and v == 1) or \
+                   (u == 3 and u == 2):
                     user += 1
                     print("Én nyertem.")
                 else:
@@ -29,22 +31,29 @@ while folytat != 'n':
             else:
                 egal += 1
                 print("Döntetlen")
-            print(f"A gép tippje: {v} volt")
+
+            gep_nyert = round(((gep/count) * 100), 3)
+            user_nyert = round(((user/count) * 100), 3)
+
+            print(f"A gép tippje: {v} volt\n")
             print(f"""Az eddig állás:
                     gép nyert: {gep} alkalommal.
                     én nyertem:{user} alkalommal.
-                    döntetlen: {egal} alkalommal.""")
+                    döntetlen: {egal} alkalommal.\n""")
+            print(f"""Az eddigi nyerési arány:
+                        gép nyert {gep_nyert} %-ban
+                        én nyertem {user_nyert} %-ban\n""")
 
             folytat = input("Akarsz még játszani?(any key for 'yes'/ 'n' for no)")
-            print('\n\n')
+            print('\n\a')
         else:
-            print("Üss 1/0/-1-et")
+            print("Üss 1 / 2 / 3-at")
             continue
     except:
-        print("Üss 1/0/-1-et")
+        print("Üss 1 / 2 / 3-at")
         continue
 
-print("Köszönöm a játékot.")
+print("Köszönöm a játékot.\n")
 
 
 
