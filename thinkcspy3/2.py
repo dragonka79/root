0a73a78d02ebf3e69c00f518c9b1867a8159b036
dragonka79 <dragonka79@gmail.com>
2022-07-25 07:54:45 +0200

    c

--- thinkcspy3/2.py
+++ thinkcspy3/2.py
@@ -1,5 +1,7 @@
-ss = "Helló, Világ!"
-
-print(ss.upper()) # Csupa nagybetű
-print(ss.lower()) # Csupa kisbetű
-print(ss.upper().capitalize()) # Mondatszerű, első betű nagy, többi kicsi
+elotag = "Törp"
+utotagok_listaja = ["erős", "költő", "morgó", "öltő", "papa", "picur", "szakáll" ]
+for utotag in utotagok_listaja:
+    if elotag[-1] != utotag[0]:
+        print(elotag + utotag)
+    else:
+        print(elotag + utotag[1:])
