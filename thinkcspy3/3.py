2cfeedd41b10158989ea6c5ea7bb33536b6cd059
dragonka79 <dragonka79@gmail.com>
2022-07-28 06:55:04 +0200

    c

--- thinkcspy3/3.py
+++ thinkcspy3/3.py
@@ -1,4 +1,10 @@
-ss = "Érdekes metódusai vannak a Python sztringeknek."
+s1 = "A nevem {0}!".format("Artúr")
+print(s1)
+nev = "Alice"
+kor = 10
+s2 = "A nevem {1}, és {0} éves vagyok.".format(kor, nev)
+print(s2)
 
-szavak = ss.split()
-print(szavak)
+
+
+print("A nevem {0}".format("Zoli"))
