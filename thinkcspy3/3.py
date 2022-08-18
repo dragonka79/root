1be0c78516245bd3999d8944d6f771f9bba33ed1
dragonka79 <dragonka79@gmail.com>
2022-08-18 17:28:41 +0200

    c

--- thinkcspy3/3.py
+++ thinkcspy3/3.py
@@ -1,10 +1,1 @@
-s1 = "A nevem {0}!".format("Artúr")
-print(s1)
-nev = "Alice"
-kor = 10
-s2 = "A nevem {1}, és {0} éves vagyok.".format(kor, nev)
-print(s2)
-
-
-
-print("A nevem {0}".format("Zoli"))
+print("ananász" < "Barack")
