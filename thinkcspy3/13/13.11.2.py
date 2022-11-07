ad712c60bfe509b70a309155dda60884069e4086
Zoltán Dienes <76064726+dragonka79@users.noreply.github.com>
2022-11-07 07:23:06 +0100

    Update 13.11.2.py

--- thinkcspy3/13/13.11.2.py
+++ thinkcspy3/13/13.11.2.py
@@ -1,6 +1,8 @@
 # Csak az adott stringet tartalmazó sorok kiíratása
 
-f = open("C:/Users/a44793837/OneDrive - Deutsche Telekom AG/python/13/13112sample.txt",)
+#### 1. Megoldás
+
+f = open("C:/Users/a44793837/OneDrive - Deutsche Telekom AG/python/13/13112sample.txt")
 j = 'info'
 
 while True:
@@ -13,5 +15,16 @@ while True:
         continue
 
 f.close()
+
+#### 2. (egyszerűbb) Megoldás
+
+f = open("C:/Users/a44793837/OneDrive - Deutsche Telekom AG/python/13/13112sample.txt")
+h = f.readlines() # a file-t soronként bepakolom egy listába
+f.close()
+j = 'info'
+
+for i in h:
+    if j in i:
+        print(i, end='')
 
 
