f7266434868eee6ac3d45ef7fccb28cf4c08ba72
Zoltán Dienes <76064726+dragonka79@users.noreply.github.com>
2022-11-13 16:06:34 +0100

    Update 14.11.4.a.py

--- thinkcspy3/14/14.11.4.a.py
+++ thinkcspy3/14/14.11.4.a.py
@@ -1,12 +1,8 @@
-
-from audioop import reverse
-
-
 # a. Írj egy függvényt, amely egy megoldást tükröz az Y tengelyre.
 
 def y_tengelyre_tukroz(list):
-    revlist = list[::-1]
-    return revlist
+    revlisty = list[::-1]
+    return revlisty
 
 x = [0,4,7,5,2,6,1,3]
 print(x)
