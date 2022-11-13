e74706383e1f3780cd31fc3eeed8a1b1a78e6514
Zoltán Dienes <76064726+dragonka79@users.noreply.github.com>
2022-11-13 16:52:21 +0100

    Update 14.11.4.a.py

--- thinkcspy3/14/14.11.4.a.py
+++ thinkcspy3/14/14.11.4.a.py
@@ -1,8 +1,8 @@
 # a. Írj egy függvényt, amely egy megoldást tükröz az Y tengelyre.
 
-def y_tengelyre_tukroz(list):
-    revlisty = list[::-1]
-    return revlisty
+def y_tengelyre_tukroz(megoldas):
+    revmegoldasy = megoldas[::-1]
+    return revmegoldasy
 
 x = [0,4,7,5,2,6,1,3]
 print(x)
