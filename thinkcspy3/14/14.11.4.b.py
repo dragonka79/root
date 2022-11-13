e37b019a1352a595074c915275bba099e740f348
Zoltán Dienes <76064726+dragonka79@users.noreply.github.com>
2022-11-13 16:06:51 +0100

    Add files via upload

---
+++ thinkcspy3/14/14.11.4.b.py
@@ -0,0 +1,12 @@
+#b. Írj egy függvényt, amely egy megoldást tükröz az X tengelyre.
+
+def x_tengelyre_tukroz(list):
+    revlistx = []
+    for i in range(len(list)):
+        k = int(7 - list[i])
+        revlistx.append(k)
+    return revlistx
+
+x = [0,4,7,5,2,6,1,3]
+print(x)
+print(x_tengelyre_tukroz(x))
