f7aaee64d1964c58d18beb5687ef0a26ec0b9d4a
dragonka79 <dragonka79@gmail.com>
2023-02-02 09:25:39 +0100

    create

--- devasc/5/parse_csv.py
+++ devasc/5/parse_csv.py
@@ -2,23 +2,23 @@
 
 import csv
 
-# datacsv = open("/home/zolcs/Downloads/little_stuff/devasc/5/routerlist.csv")
-# datacsvreader = csv.reader(datacsv)
-# csvpy = list(datacsvreader)
-# datacsv.close()
+datacsv = open("/home/zolcs/Downloads/little_stuff/devasc/5/routerlist.csv")
+datacsvreader = csv.reader(datacsv)
+csvpy = list(datacsvreader)
+datacsv.close()
 
 # print(f"\n{csvpy}\n")
 
 ## 1.2 Displaying csv in a more user friendly manner
 
-# with open("/home/zolcs/Downloads/little_stuff/devasc/5/routerlist.csv") as data:
-#     csv_list = csv.reader(data)
-#     for row in csv_list:
-#         device = row[0]
-#         ip = row[1]
-#         location = row[2]
-#         print(f"{device} is in {location.rstrip()} and has IP {ip}.")
-#     print("\t")
+with open("/home/zolcs/Downloads/little_stuff/devasc/5/routerlist.csv") as data:
+    csv_list = csv.reader(data)
+    for row in csv_list:
+        device = row[0]
+        ip = row[1]
+        location = row[2]
+        print(f"{device} is in {location.rstrip()} and has IP {ip}.")
+    print("\t")
 
 ## The rstrip() method removes any characters at the end a string,
 ## space is the default trailing character to remove.
@@ -44,5 +44,7 @@ with open("/home/zolcs/Downloads/little_stuff/devasc/5/routerlist.csv") as data:
         location = row[2]
         print(f"{device} is in {location.rstrip()} and has IP {ip}.")
     print("\t")
+
+
 
 
