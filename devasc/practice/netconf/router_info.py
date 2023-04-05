ed801aad56354526a59db89b4d7f578c5c0fe421
Zoltan Dienes <dragonka79@gmail.com>
2023-04-05 10:41:20 +0200

    Fixed

--- devasc/practice/netconf/router_info.py
+++ devasc/practice/netconf/router_info.py
@@ -1,4 +1,4 @@
 router = {"host": "sandbox-iosxe-recomm-1.cisco.com",
           "port": "830",
           "username": "developer",
-          "password": "C1sco12345"}
+          "password": "lastorangerestoreball8876"}
