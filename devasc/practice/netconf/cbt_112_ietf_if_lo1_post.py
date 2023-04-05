f0aec407b31faab9b179cd9723c5bb0f8de76f1f
Zoltan Dienes <dragonka79@gmail.com>
2023-04-05 16:55:18 +0200

    create

---
+++ devasc/practice/netconf/cbt_112_ietf_if_lo1_post.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+url = "https://sandbox-iosxe-latest-1.cisco.com:443/restconf/data/ietf-interfaces:interfaces"
+
+payload = json.dumps({
+    "ietf-interfaces:interface": [
+        {
+            "name": "Loopback1",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {},
+            "ietf-ip:ipv6": {}
+        }
+    ]
+})
+headers = {
+    'Accept': 'application/yang-data+json',
+    'Content-Type': 'application/yang-data+json',
+    'Authorization': 'Basic YWRtaW46QzFzY28xMjM0NQ=='
+}
+
+response = requests.request(
+    "POST", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
