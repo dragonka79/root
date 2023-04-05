174ba7731fc4782a147e386c07fe1e4bb47f1f67
Zoltan Dienes <dragonka79@gmail.com>
2023-04-05 12:20:19 +0200

    Create

---
+++ devasc/practice/netconf/netconf-editor_port.py
@@ -0,0 +1,12 @@
+from ncclient import manager
+from router_info import router
+
+config_template = open(
+    "/home/zolcs/git/little_tasks/devasc/practice/netconf/ios_config_port.xml").read()
+
+netconf_config = config_template.format(
+    interface_name="GigabitEthernet2", if_value="true")
+
+with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+    device_reply = m.edit_config(netconf_config, target="running")
+    print(device_reply)
