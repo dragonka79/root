dc09537943913610750f1abaa0290e3ddad5ce7d
Zoltan Dienes <dragonka79@gmail.com>
2023-04-05 11:49:59 +0200

    if desc set to the original

--- devasc/practice/netconf/netconf-editconfig.py
+++ devasc/practice/netconf/netconf-editconfig.py
@@ -2,10 +2,10 @@ from ncclient import manager
 from router_info import router
 
 config_template = open(
-    "/home/zolcs/Dropbox/python/devasc/NETCONF/ios_config.xml").read()
+    "/home/zolcs/git/little_tasks/devasc/practice/netconf/ios_config.xml").read()
 
 netconf_config = config_template.format(
-    interface_name="GigabitEthernet2", interface_desc="daniyal")
+    interface_name="GigabitEthernet2", interface_desc="Network Interface")
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
     device_reply = m.edit_config(netconf_config, target="running")
