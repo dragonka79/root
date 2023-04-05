147a94eba128d1c27d44628183d0767426c1df74
Zoltan Dienes <dragonka79@gmail.com>
2023-04-05 16:57:15 +0200

    create

---
+++ devasc/practice/netconf/cbt_112_ietf_if_lo1_put.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+url = "https://sandbox-iosxe-latest-1.cisco.com:443/restconf/data/ietf-interfaces:interfaces/interface=Loopback1"
+
+payload = json.dumps({
+    "ietf-interfaces:interface": [
+        {
+            "name": "Loopback1",
+            "type": "iana-if-type:softwareLoopback",
+            "description": "dragonka was here",
+            "ietf-ip:ipv4": {},
+            "ietf-ip:ipv6": {}
+        }
+    ]
+})
+headers = {
+    'Accept': 'application/yang-data+json',
+    'Content-Type': 'application/yang-data+json',
+    'Authorization': 'Basic YWRtaW46QzFzY28xMjM0NQ=='
+}
+
+response = requests.request(
+    "PUT", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
