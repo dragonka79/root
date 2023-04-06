dd8789ba8bb38beacdf3568b37752dea33adf708
Zoltan Dienes <dragonka79@gmail.com>
2023-04-06 07:46:00 +0200

    Create

---
+++ devasc/practice/netconf/NX-API/NX-API_GET.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+url = "https://sbx-nxos-mgmt.cisco.com:443/ins"
+
+payload = json.dumps({
+    "ins_api": {
+        "version": "1.0",
+        "type": "cli_show",
+        "chunk": "0",
+        "sid": "sid",
+        "input": "show version ;show ip int br ;show int status",
+        "output_format": "json"
+    }
+})
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic YWRtaW46QWRtaW5fMTIzNCE=',
+    'Cookie': 'nxapi_auth=dzqnf:VjCNmVhRFUDkLkNvDo2/UfGvBnw='
+}
+
+response = requests.request(
+    "POST", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
