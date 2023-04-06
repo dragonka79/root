45256ead61b0aa1c3fbe5f91ab58e680923c458b
Zoltan Dienes <dragonka79@gmail.com>
2023-04-06 08:45:15 +0200

    Create

---
+++ devasc/practice/NX-API/NX-API_GET_2.py
@@ -0,0 +1,32 @@
+# This is the NX-API_GET equivalent
+# Getting some show commands from NX-API Cisco sandbox
+
+import requests
+import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+target = "https://sbx-nxos-mgmt.cisco.com:443/ins"
+username = 'admin'
+password = 'Admin_1234!'
+
+requestheaders = {'content-type': 'application/json'}
+showcmd = {
+    "ins_api": {
+        "version": "1.0",
+        "type": "cli_show",
+        "chunk": "0",
+        "sid": "sid",
+        "input": "show version ;show ip int br ;show int status",
+        "output_format": "json"
+    }
+}
+response = requests.post(
+    target,
+    data=json.dumps(showcmd),
+    headers=requestheaders,
+    auth=(username, password),
+    verify=False,
+).json()
+
+print(json.dumps(response, indent=2, sort_keys=True))
