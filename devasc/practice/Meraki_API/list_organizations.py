7add766ec0e91daf7d5af06185ae9a2b13c9b76a
Zoltan Dienes <dragonka79@gmail.com>
2023-04-07 08:56:14 +0200

    added 4.

--- devasc/practice/Meraki_API/list_organizations.py
+++ devasc/practice/Meraki_API/list_organizations.py
@@ -1,7 +1,8 @@
 # 1. From the code set in Postman under Meraki Dashboard API/list_organizations
 # + Same cosmetic upgrade for better readability
 # 2. + Getting a Id of an organization from the response -> Meraki Dashboard API/list_networks_by_orgid
-# 3. + Getting network on an organization
+# 3. + Getting network on an organization -> Meraki Dashboard API/list_networks_by_orgid
+# 4. Obtain a list of devices in a given network -> Meraki Dashboard API/list_devices_in_a_network
 
 import json
 import requests
@@ -18,7 +19,7 @@ headers = {
 # Save the response to a var and converting it to a python dict
 response = requests.request("GET", url, headers=headers, data=payload).json()
 
-print(json.dumps(response, indent=2, sort_keys=True))
+# print(json.dumps(response, indent=2, sort_keys=True))
 
 # 2. Getting and printing the id for 'DeLab' organization
 
@@ -26,7 +27,7 @@ for response_org in response:
     if response_org['name'] == 'DeLab':
         orgId = response_org['id']
 
-print(orgId)
+# print(orgId)
 
 # 3. List of networks of the orgId
 
@@ -40,4 +41,23 @@ url2 = baseURI + org + orgId + net
 response_network = requests.request(
     "GET", url2, headers=headers, data=payload).json()
 
-print(json.dumps(response_network, indent=2))
+# print(json.dumps(response_network, indent=2))
+
+# 4. Obtain a list of devices in a given network
+
+for response_net in response_network:
+    if response_net['name'] == 'DevNetLab':
+        networkId = response_net['id']
+
+
+url = "https://api.meraki.com/api/v1/networks/L_783626335162466320/devices"
+
+org2 = "/networks/"
+dev = "/devices"
+
+url3 = baseURI + org2 + networkId + dev
+
+response_devices = requests.request(
+    "GET", url3, headers=headers, data=payload).json()
+
+print(json.dumps(response_devices, indent=2))
