c030215926ee6e2c9ebae24c8687b84a10875fc4
Zoltan Dienes <dragonka79@gmail.com>
2023-04-07 10:31:18 +0200

    Create

---
+++ devasc/practice/Meraki_API/SDK_list_org.py
@@ -0,0 +1,23 @@
+# List the organizations that the user has privileges on
+
+import meraki
+import json
+from pprint import pprint
+
+# Defining your API key as a variable in source code is not recommended
+API_KEY = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+# Instead, use an environment variable as shown under the Usage section
+# @ https://github.com/meraki/dashboard-api-python/
+
+dashboard = meraki.DashboardAPI(API_KEY)
+
+
+orgs = dashboard.organizations.getOrganizations()
+
+pprint(orgs)
+
+for org in orgs:
+    if org['name'] == 'DeLab':
+        orgID = org['id']
+
+print(orgID)
