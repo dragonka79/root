ac2053903013ec0217206f702df7a3aa6dfd3a79
Zoltan Dienes <dragonka79@gmail.com>
2023-04-08 18:58:32 +0200

    set get apn part

--- devasc/practice/4.8_Secu/ACI_request.py
+++ devasc/practice/4.8_Secu/ACI_request.py
@@ -16,11 +16,29 @@ payload = {
     }
 }
 headers = {
-    'Content-Type': 'application/json',
-    # 'Cookie': 'APIC-cookie=eyJhbGciOiJSUzI1NiIsImtpZCI6Imc0ZHZ4eTA1emtqNmZhZnoxZjI5MHlkcnozYnUyYjAzIiwidHlwIjoiand0In0.eyJyYmFjIjpbeyJkb21haW4iOiJhbGwiLCJyb2xlc1IiOjAsInJvbGVzVyI6MX1dLCJpc3MiOiJBQ0kgQVBJQyIsInVzZXJuYW1lIjoiYWRtaW4iLCJ1c2VyaWQiOjE1Mzc0LCJ1c2VyZmxhZ3MiOjAsImlhdCI6MTY4MDk2OTQ3MCwiZXhwIjoxNjgwOTcwMDcwLCJzZXNzaW9uaWQiOiIzMkppT29qZ1IrMkJYNXY4QXJ2eUZ3PT0ifQ.f-ljjQg1mrUoLW2CcgSjaUUFVeMOe2v-SyL7kZjJmknUhFwjxN76VoifJddLYe6vBr96Q6velISJT20yyDN47Kpx2qZawZkrzvtfQrFxfJ39Lt2V9wF59yPA2kRZfFlsMDhwWkWNsLXktJ6GiWwspuHgukw6OngVf9psaKt9jkFUZmcJMJr85qOW4zsQIs95v1O0JSaRWFhJr-cZr5O9E3m4iiUhpsF0ZLaAt2-_8hy8pDg2eocXa3rv9QZXHWmAnzroagz3rfcXfw3z4i0pyY0PJ-nckbEIiVLgYKGWeovzg_6JKDUgt-OyBPYY1kiR2hh76K102OaLZVs2F1qpew'
+    'Content-Type': 'application/json'
 }
 
 response = requests.request(
     "POST", url, headers=headers, data=json.dumps(payload), verify=False).json()
 
 print(json.dumps(response, indent=2))
+
+####### Parse token and set cookie ############
+
+token = response['imdata'][0]['aaaLogin']['attributes']['token']
+cookie = {}
+cookie['APIC-cookie'] = token
+
+####### GET the Application Profile ###########
+
+url = "https://sandboxapicdc.cisco.com:443/api/node/mo/uni/tn-PAWAN/ap-AP1.json"
+
+headers = {
+    'cache-control': 'no-cache'
+}
+
+get_response = requests.request(
+    "GET", url, headers=headers, cookies=cookie, verify=False).json()
+
+print(json.dumps(get_response, indent=2))
