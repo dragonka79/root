418371eb2e6eaa272aa216fbcc266aa714710b8e
Zoltan Dienes <dragonka79@gmail.com>
2023-04-09 08:52:53 +0200

    typo fixed

--- devasc/practice/Unit_test_TDD/Unit_example.py
+++ devasc/practice/Unit_test_TDD/Unit_example.py
@@ -2,13 +2,13 @@ import math
 import unittest
 
 
-def calcCircumfrence(r):
+def calcCircumference(r):
     return r * 2 * math.pi
 
 
 class TestCode(unittest.TestCase):
     def test_circumfrence(self):
-        self.assertAlmostEqual(calcCircumfrence(1), 6.28318530)
+        self.assertAlmostEqual(calcCircumference(1), 6.28318530)
 
 
 unittest.main()
