824fbce95ebaa6b29d7edcb4623d539a678b5047
Zoltan Dienes <dragonka79@gmail.com>
2023-06-29 20:26:55 +0200

    c

--- thinkcspy3/20/1.py
+++ thinkcspy3/20/1.py
@@ -19,10 +19,10 @@ hun2esp2 = {'egy': 1, 'kettő': 'dos'}
 # print(type(hun2esp2))
 
 # del hun2esp2['egy']
-hun2esp2['egy'] += 2
+# hun2esp2['egy'] += 2
 
 
-hun2esp2['négy'] = 'quatros'
+# hun2esp2['négy'] = 'quatros'
 # print(hun2esp2)
 
 # for k in hun2esp2.keys():
@@ -31,6 +31,38 @@ print('\t')
 # for k in hun2esp2:
 #     print(f'A(z) {k} metódus leképezi a {hun2esp2[k]} értéket')
 
-print(list(hun2esp2.keys()))
-print(list(hun2esp2.values()))
-print(list(hun2esp2.items()))
+# print(list(hun2esp2.keys()))
+# print(list(hun2esp2.values()))
+# print(list(hun2esp2.items()))
+
+# print('egy' in hun2esp2)
+# print('egy' in hun2esp2.keys())
+# print('dos' in hun2esp2.values())
+
+# hun2esp3 = hun2esp2
+# hun2esp4 = hun2esp2.copy()
+
+# print(hun2esp2 is hun2esp3)
+# print(hun2esp2 is hun2esp4)
+
+# hun2esp2["öt"] = 'cing'
+# print(hun2esp2, hun2esp3, hun2esp4)
+
+
+# list2 = [(1, 'egy'), (2, 'kettő')]
+# list3 = list2
+# list4 = list2.copy()
+# print(list3 is list2)
+# print(list4 is list2)
+
+known_elements = {0: 0, 1: 1}
+
+
+def fib(n):
+    if n not in known_elements:
+        known_elements[n] = fib(n-2) + fib(n-1)
+    return known_elements[n]
+
+
+n = 100
+print(fib(n))
