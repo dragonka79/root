487e291fc94043c72f0be183013fc07f3ad283c0
Zoltan Dienes <dragonka79@gmail.com>
2023-06-30 07:50:19 +0200

    c

--- thinkcspy3/20/20.8.1.py
+++ thinkcspy3/20/20.8.1.py
@@ -1,15 +1,14 @@
+ABC = "abcdefghijklmnopqrstuvwxyz0123456789"
 string_text = "Ez a Sztring Kis es Nagy Betuket tartalmaz.0."
 string_text_lower = string_text.lower()
 betu_szamlalo = {}
 for betu in string_text.lower():
-    betu_szamlalo[betu] = betu_szamlalo.get(betu, 0) + 1
+    if betu in ABC:
+        betu_szamlalo[betu] = betu_szamlalo.get(betu, 0) + 1
 
 betuk = list(betu_szamlalo.items())
 betuk.sort()
 
-ABC = "abcdefghijklmnopqrstuvwxyz0123456789"
-
 for b in betuk:
     if b[0] in ABC:
         print(f"{b[0]}  {b[1]}")
-
