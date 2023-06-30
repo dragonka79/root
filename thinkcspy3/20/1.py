c90cce90200118fc9428c4645350ce4eeff41939
Zoltan Dienes <dragonka79@gmail.com>
2023-06-30 07:50:40 +0200

    c

--- thinkcspy3/20/1.py
+++ thinkcspy3/20/1.py
@@ -1,6 +1,14 @@
-sztring = "Missi ssippi"
+ABC = "abcdefghijklmnopqrstuvwxyz0123456789"
+string_text = "Ez a Sztring Kis es Nagy Betuket tartalmaz.0."
+string_text_lower = string_text.lower()
 betu_szamlalo = {}
-for betu in sztring:
-    betu_szamlalo[betu] = betu_szamlalo.get(betu, 0) + 1
+for betu in string_text.lower():
+    if betu in ABC:
+        betu_szamlalo[betu] = betu_szamlalo.get(betu, 0) + 1
 
-print(betu_szamlalo)
+betuk = list(betu_szamlalo.items())
+betuk.sort()
+
+for b in betuk:
+    if b[0] in ABC:
+        print(f"{b[0]}  {b[1]}")
